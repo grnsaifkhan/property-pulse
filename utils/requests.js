9ee7e67ace7b1662b87c2ed0a33a32b975d1f19e
grnsaifkhan <saifkhan110180@gmail.com>
2024-06-17 16:09:38 +0200

    seperated the fecthProerties function. and integrated the function with properties page and HomeProperties file

---
+++ utils/requests.js
@@ -0,0 +1,25 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN  || null;
+
+async function fetchProperties() {
+    try {
+
+      //Handle the case wheer the domain is not available
+      if(!apiDomain){
+        return [];
+      }
+
+      const res = await fetch(`${apiDomain}/properties`);
+  
+      if(!res.ok) {
+        throw new Error('Failed to fetch data');
+      }
+  
+      return res.json();
+    } catch (error) {
+      console.log(error);
+
+      return [];
+    }
+  }
+
+  export {fetchProperties};
