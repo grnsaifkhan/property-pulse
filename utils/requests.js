4645334c7282c153cbae5d39c9426487747c29fe
grnsaifkhan <saifkhan110180@gmail.com>
2024-06-18 12:24:52 +0200

    Added new function to fetch single property detais

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,7 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN  || null;
 
+
+//Fetch al properties
 async function fetchProperties() {
     try {
 
@@ -22,4 +24,28 @@ async function fetchProperties() {
     }
   }
 
-  export {fetchProperties};
+  //Fetch single property
+
+  async function fetchProperty(id) {
+    try {
+
+      //Handle the case wheer the domain is not available
+      if(!apiDomain){
+        return null;
+      }
+
+      const res = await fetch(`${apiDomain}/properties/${id}`);
+  
+      if(!res.ok) {
+        throw new Error('Failed to fetch data');
+      }
+  
+      return res.json();
+    } catch (error) {
+      console.log(error);
+
+      return null;
+    }
+  }
+
+  export {fetchProperties, fetchProperty};
