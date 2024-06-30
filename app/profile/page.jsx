86bfd549d03c592e09eda5a806eb22d6188d45d4
grnsaifkhan <saifkhan110180@gmail.com>
2024-06-30 18:42:48 +0200

    funciton to delete property using delete api

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -45,7 +45,32 @@ const ProfilePage = () => {
         } 
     }, [session]);
 
-    const handleDeleteProperty = () => {};
+    const handleDeleteProperty = async (propertyId) => {
+        const confirmed = window.confirm('Are yo sure you want to delete this property?');
+
+        if(!confirmed) {
+            return;
+        }
+
+        try {
+            const res = await fetch(`/api/properties/${propertyId}`,{ method: 'DELETE'});
+
+            if(res.status === 200) {
+                // Remove the property from state
+                const updatedProperties = properties.filter((property) => property._id !== propertyId);
+
+                setProperties(updatedProperties);
+
+                alert('property Deleted');
+            } else {
+                alert('Failed to delete property')
+            }
+        } catch (error) {
+            console.log(error);
+            alert('Failed to delete property')
+            
+        }
+    };
 
 
   return (
