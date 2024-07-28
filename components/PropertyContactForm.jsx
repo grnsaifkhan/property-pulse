58111e87fb97eab35b2dc0676c788139a529e9e7
grnsaifkhan <saifkhan110180@gmail.com>
2024-07-28 19:56:35 +0200

    INtegrated the message api to property contact form to send message

--- components/PropertyContactForm.jsx
+++ components/PropertyContactForm.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { useState } from 'react';
 import { FaPaperPlane } from 'react-icons/fa'
+import {toast} from 'react-toastify';
 
 const PropertyContactForm = ({property}) => {
   const [name, setName] = useState('');
@@ -10,7 +11,7 @@ const PropertyContactForm = ({property}) => {
   const [phone, setPhone] = useState('');
   const [wasSubmitted, setWasSubmitted] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const data = {
@@ -22,9 +23,32 @@ const PropertyContactForm = ({property}) => {
       property: property._id
     };
 
-    console.log(data);
+    try {
+      const res = await fetch('/api/messages', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
 
-    setWasSubmitted(true);
+      if(res.status === 200) {
+        toast.success('Message sent successfully');
+        setWasSubmitted(true);
+      } else if(res.status === 400 || res.status === 401) {
+        toast.error(data.message);
+      } else {
+        toast.error('Error sending form');
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Error sending form');
+    }finally{
+      setName('');
+      setEmail('');
+      setPhone('');
+      setMessage('');
+    }
   }
   return (
     <div className="bg-white p-6 rounded-lg shadow-md">
