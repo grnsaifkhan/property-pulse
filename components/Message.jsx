6ae242af7504cd82fdf4eb9abc2107f91da0dedf
grnsaifkhan <saifkhan110180@gmail.com>
2024-08-27 20:04:05 +0200

    added unread message count notification through global provider context api

--- components/Message.jsx
+++ components/Message.jsx
@@ -2,10 +2,13 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
+import { useGlobalContext } from '@/context/GlobalContext';
 
 const Message = ({message}) => {
   const [isRead, setIsRead] = useState(message.read);
   const [isDeleted, setIsDeleted] = useState(false);
+
+  const {unreadMessageCount, setUnreadMessageCount} = useGlobalContext();
 
   const handleReadClick = async () => {
     try {
@@ -16,6 +19,7 @@ const Message = ({message}) => {
       if(res.status === 200) {
         const {read} = await res.json();
         setIsRead(read);
+        setUnreadMessageCount((prevCount) => (read ? prevCount - 1 : prevCount + 1));
 
         if(read) {
           toast.success('Marked as read');
@@ -37,6 +41,7 @@ const Message = ({message}) => {
 
       if(res.status === 200){
         setIsDeleted(true);
+        setUnreadMessageCount((prevCount) => prevCount - 1);
         toast.success('Message Deleted');
       }
     } catch (error) {
